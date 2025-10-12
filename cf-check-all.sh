8618586911c881132b4f178c4f2f86c08735409e
mael/splits <145584123+maelm-Epita@users.noreply.github.com>
2025-10-12 12:44:06 +0200

    Update cf-check-all.sh

--- cf-check-all.sh
+++ cf-check-all.sh
@@ -3,3 +3,7 @@
 for file in $(find . -name \*.c -print); do
   diff <(clang-format "$file") <(cat "$file")
 done
+
+for file in $(find . -name \*.h -print); do
+  diff <(clang-format "$file") <(cat "$file")
+done
